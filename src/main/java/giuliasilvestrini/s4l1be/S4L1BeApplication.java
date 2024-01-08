8df1730ea5e5a08453b2a99221f2b9e4e0cf007e
Giulia Silvestrini <silvergiuly@hotmail.it>
2024-01-08 14:15:52 +0100

    Initial commit

---
+++ src/main/java/giuliasilvestrini/s4l1be/S4L1BeApplication.java
@@ -0,0 +1,13 @@
+package giuliasilvestrini.s4l1be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S4L1BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S4L1BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giuliasilvestrini/s4l1be/S4L1BeApplicationTests.java
@@ -0,0 +1,13 @@
+package giuliasilvestrini.s4l1be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S4L1BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
